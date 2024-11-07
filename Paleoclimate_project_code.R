f60845f227d12987a5832dfa950e04ef27c10797
embyrne0 <eleanormargo.byrne@gmail.com>
2024-11-07 14:47:35 -0500

    code for looking at data

---
+++ Paleoclimate_project_code.R
@@ -0,0 +1,9 @@
+# Paleoclimate Project
+
+# Install packages
+  # install.packages("readxl")
+library("readxl")
+
+
+# Open the excel file
+benthic_foram <- read_excel("C:/Users/Margo/OneDrive/Documents/Fall 2024 Courses/PaleoClimate_course/Data/Census counts of benthic foraminifera.xlsx")
